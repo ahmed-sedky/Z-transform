31bd60fc050f2dbdbeec5c43cc30e65283b4459d
ahmed-sedky <61352734+ahmed-sedky@users.noreply.github.com>
2021-06-05 20:53:42 +0200

    unit circle done

--- main.py
+++ main.py
@@ -2,37 +2,61 @@ from flask import Flask, render_template ,request,url_for,redirect,session #impo
 from scipy.interpolate import interp1d
 # m = interp1d([485,1020],[-1,1])
 # n = interp1d([265,800],[-1,1])
-m = interp1d([375,1135],[-1.5,1.5])
-n = interp1d([170,915],[-1.5,1.5])
+m = interp1d([375,1141],[-1.5,1.5])
+n = interp1d([298,1028],[-1.5,1.5])
 
 app = Flask(__name__) 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SECRET_KEY'] = "secret"
 
 
-@app.route('/pole',methods= ['POST','GET'])
-def zero_pole():
-    if request.method == 'POST':
-            status      = request.form['pole_zero']
-    return render_template('main.html',status =[status])
+# @app.route('/pole',methods= ['POST','GET'])
+# def zero_pole():
+#     if request.method == 'POST':
+#             status      = request.form['pole_zero']
+#     return render_template('main.html',status =[status])
 @app.route('/',methods= ['POST','GET'])
 def index():
     if request.method == 'POST':
-            status      = request.form['pole_zero']
-            locZx        = request.form['locZx']
-            locZy        = request.form['locZy']
-            locPx        = request.form['locPx']
-            locPy        = request.form['locPy']
-            
-            locationZx   = m(locZx)
-            locationZy   = n(locZy)
-            locationPx   = m(locPx)
-            locationPy   = n(locPy)
-            print(status)
-            print(locationZx)
-            print(locationZy)
-            print(locationPx)
-            print(locationPy)
+            countP      = int(request.form['hidden_countP'])
+            countZ      = int(request.form['hidden_countZ'])
+            print(countP)
+            print(countZ)
+            # locPx        = request.form['locPx1']
+            locPx        =request.form['locPx'+ str("1")]
+            print (locPx)
+            for i in range (1,countP+1):
+                print("htis is i ",i)
+                # locZx        = int(request.form['locZx' + i])
+                # locZy        = int(request.form['locZy'+ i])
+                locPx        = int(request.form['locPx'+ str(i)])
+                locPy        = int(request.form['locPy'+ str(i)])
+                # print(locPx)
+                # print(locPy)
+                # locationZx   = m(locZx+ i)
+                # locationZy   = n(locZy+ i)
+                locationPx   = m(locPx+ i)
+                locationPy   = n(locPy+ i)
+                # print(locationZx)
+                # print(locationZy)
+                print(locationPx)
+                print(locationPy)
+            for i in range (1,countZ+1):
+                print("htis is i ",i)
+                locZx        = int(request.form['locZx' + str(i)])
+                locZy        = int(request.form['locZy'+ str(i)])
+                # locPx        = int(request.form['locPx'+ i])
+                # locPy        = int(request.form['locPy'+ i])
+                # print(locZx)
+                # print(locZy)
+                locationZx   = m(locZx+ i)
+                locationZy   = n(locZy+ i)
+                # locationPx   = m(locPx+ i)
+                # locationPy   = n(locPy+ i)
+                print(locationZx)
+                print(locationZy)
+                # print(locationPx)
+                # print(locationPy)
             # status      = request.form['pole_zero']
             # countP        = request.form['count1']
             # countZ        = request.form['count2']
--- static/script/main2.js
+++ static/script/main2.js
@@ -1,64 +1,135 @@
 var countP = 0 ;
 var countZ = 0 ;
-$("#addPole").click(function(e){
+var input =1;
+var last_div = "";
+$("#addPole").on("click" ,function(){input =1;
+    $(last_div).unbind()
+ });
+$(document).on('click','#addPole',function(e){
     // $("#uCircle").addClass("active")
     countP= countP+1;
-    console.log(countP);
-    $(".x").after(    '<div class='+"hidden x"+countP+'>x</div>'    )
-    $("#hiddenPole").val(countP)
+    $(".x").after(        '<div class='+'"hidden pole '+ 'x'+countP+'">x</div>'    )
+    last_div = (".x"+countP+"")
+    console.log("before" + countP)
+    
+        $(document).on('click','#uCircle',function(p){
+            if( input == 1)
+            {
+            console.log("pole")
+        // $("#uCircle").click(function(e){
+            $(".x"+countP+"").css("display", "block").offset({  //offset bt77dle el div hyzhr fen
+                        left: p.pageX-5  ,
+                        top: p.pageY-20 
+                    });
+            console.log((".x"+countP+""))
+            console.log(input)
+            // $("#hiddenZero").val(countZ)
+            $("#hiddenPx").after('<input class='+"hiddenPx"+countP+' " type="hidden" value = "0"  name='+"locZx"+countP+'>')
+            $("#hiddenPy").after('<input class='+"hiddenPy"+countP+' " type="hidden" value = "0"  name='+"locZy"+countP+'>')
+            $(".hiddenPx"+countP+"").val(p.pageX);
+            $(".hiddenPy"+countP+"").val(p.pageY);
+            console.log("#hiddenPx"+countP+"")
+            }   
+        });
 });
-$("#addZero").click(function(e){
+$("#addZero").on("click" ,function(){
+    input =0 ;
+    $(last_div).unbind()
+});
+$(document).on('click','#addZero',function(e){
+// $("#addZero").click(function(e){
     // $("#uCircle").addClass("active")
     countZ= countZ+1;
-    $(".circle").after(        '<div class='+"hidden circle"+countZ+'></div>'    )
-    $("#hiddenZero").val(countZ)
-})
-$(document).on('change','#pole_zero',function(){
-    var input = $("#pole_zero").val(); 
-    console.log(input);
-    $("#uCircle").click(function(e){
-        console.log("count poles" +countP);
-        console.log("count zeros" +countZ);
-        if (input == "zero")
-        {
-            console.log("hey")
-            for (var i = 0 ;i <countZ ; i++){
-                console.log("hossam")
-                $(".circle"+i+"").css("display", "block").offset({  //offset bt77dle el div hyzhr fen
-                    left: e.pageX  ,
-                    top: e.pageY 
-                });
-                $("#hiddenZx").after('<input id='+"hiddenZx"+i+' class="visually-hidden " type="text" value="0" name='+"locZx"+i+'>')
-                $("#hiddenZy").after('<input id='+"hiddenZy"+i+' class="visually-hidden " type="text" value="0" name='+"locZx"+i+'>')
-                $("#hiddenZx"+i+"").val(e.pageX);
-                $("#hiddenZy"+i+"").val(e.pageY);
-                console.log(input)
-                console.log("pagex is: "+ e.pageX + " & pageY is: " + e.pageY)
-            }
-            
-        }else if (input == "pole")
-        {   
-            console.log("hey2")
-            for (var i = 0 ; i< $("#hiddenPole").val() ; i++)
+    $(".circle").after(        '<div class='+'"hidden cir '+ 'circle'+countZ+'"></div>'    )
+    console.log("before" + countZ)
+    last_div = (".circle"+countZ+"")
+        $(document).on('click','#uCircle',function(z){
+            if(input ==0)
             {
-                console.log("ahmed")
-                $(".x" + i).css("display", "block").offset({  //offset bt77dle el div hyzhr fen
-                    left: e.pageX,
-                    top: e.pageY-30 
-                });
-                $("#hiddenPx").val(e.pageX);
-                $("#hiddenPy").val(e.pageY);
-            }
-        }
-        else{
-            $(".circle").fadeIn(1000).offset({  //offset bt77dle el div hyzhr fen
-                left: e.pageX  ,
-                top: e.pageY 
-            });
-            $("#hiddenZx").val(e.pageX);
-            $("#hiddenZy").val(e.pageY);
+            console.log("zero")
             console.log(input)
-            console.log("pagex is: "+ e.pageX + " & pageY is: " + e.pageY)
-        }
-    });
-});  
+        // $("#uCircle").click(function(e){
+            $(".circle"+countZ+"").css("display", "block").offset({  //offset bt77dle el div hyzhr fen
+                        left: z.pageX  ,
+                        top: z.pageY 
+                    });
+            console.log((".circle"+countZ+""))
+            // $("#hiddenZero").val(countZ)
+            $("#hiddenZx").after('<input class='+"hiddenZx"+countZ+' " type="hidden" value="0" name='+"locPx"+countZ+'>')
+            $("#hiddenZy").after('<input class='+"hiddenZy"+countZ+' " type="hidden" value="0" name='+"locPy"+countZ+'>')
+            $(".hiddenZx"+countZ).val(z.pageX);
+            $(".hiddenZy"+countZ).val(z.pageY);
+            console.log("after" + countZ)
+            }
+
+        });
+});
+$("form").submit(function(e){
+    $("#hidden_countZ").val(countZ);
+    $("#hidden_countP").val(countP);
+});
+//$("#hiddenx2").val(e.pageX);
+//     //             $("#hiddeny2").val(e.pageY);
+    // for (var i = 0 ;i <countZ ; i++)
+    // {
+    //     console.log("hossam")
+    //     $(".circle"+i+"").css("display", "block").offset({  //offset bt77dle el div hyzhr fen
+    //         left: e.pageX  ,
+    //         top: e.pageY 
+    //     });
+    //     $("#hiddenZx").after('<input id='+"hiddenZx"+i+' " type="text" value="0" name='+"locZx"+i+'>')
+    //     $("#hiddenZy").after('<input id='+"hiddenZy"+i+' " type="text" value="0" name='+"locZx"+i+'>')
+    //     $("#hiddenZx"+i+"").val(e.pageX);
+    //     $("#hiddenZy"+i+"").val(e.pageY);
+    //     // console.log(input)
+    //     console.log("pagex is: "+ e.pageX + " & pageY is: " + e.pageY)
+    // }
+// $(document).on('change','#pole_zero',function(){
+//     var input = $("#pole_zero").val(); 
+//     console.log(input);
+//     $("#uCircle").click(function(e){
+//         console.log("count poles" +countP);
+//         console.log("count zeros" +countZ);
+//         if (input == "zero")
+//         {
+//             console.log("hey")
+//             for (var i = 0 ;i <countZ ; i++){
+//                 console.log("hossam")
+//                 $(".circle"+i+"").css("display", "block").offset({  //offset bt77dle el div hyzhr fen
+//                     left: e.pageX  ,
+//                     top: e.pageY 
+//                 });
+//                 $("#hiddenZx").after('<input id='+"hiddenZx"+i+' " type="text" value="0" name='+"locZx"+i+'>')
+//                 $("#hiddenZy").after('<input id='+"hiddenZy"+i+' " type="text" value="0" name='+"locZx"+i+'>')
+//                 $("#hiddenZx"+i+"").val(e.pageX);
+//                 $("#hiddenZy"+i+"").val(e.pageY);
+//                 console.log(input)
+//                 console.log("pagex is: "+ e.pageX + " & pageY is: " + e.pageY)
+//             }
+            
+//         }else if (input == "pole")
+//         {   
+//             console.log("hey2")
+//             for (var i = 0 ; i< $("#hiddenPole").val() ; i++)
+//             {
+//                 console.log("ahmed")
+//                 $(".x" + i).css("display", "block").offset({  //offset bt77dle el div hyzhr fen
+//                     left: e.pageX,
+//                     top: e.pageY-30 
+//                 });
+//                 $("#hiddenPx").val(e.pageX);
+//                 $("#hiddenPy").val(e.pageY);
+//             }
+//         }
+//         else{
+//             $(".circle").fadeIn(1000).offset({  //offset bt77dle el div hyzhr fen
+//                 left: e.pageX  ,
+//                 top: e.pageY 
+//             });
+//             $("#hiddenZx").val(e.pageX);
+//             $("#hiddenZy").val(e.pageY);
+//             console.log(input)
+//             console.log("pagex is: "+ e.pageX + " & pageY is: " + e.pageY)
+//         }
+//     });
+// });  
